3ae42ce8294f288174af7767861693a2194cf29e
Ranum99 <aleksanderranum99@hotmail.com>
2019-10-15 12:19:51 +0200

    laget cupController

--- gruppeeksamen/src/Controller/CupController.java
+++ gruppeeksamen/src/Controller/CupController.java
@@ -1,4 +1,17 @@
 package Controller;
 
+import Data.Arrangementer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class CupController {
+    @FXML
+    private Label cupNavn;
+
+    @FXML
+    public void initialize() {
+        cupNavn.setText("l");
+    }
+
+
 }

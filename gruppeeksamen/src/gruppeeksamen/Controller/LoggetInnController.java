c8ac676e52bcaa7e82d62309a9bdbebb380629e0
Ranum99 <aleksanderranum99@hotmail.com>
2019-10-30 15:08:55 +0100

    lettere å teste legge til lag?

--- gruppeeksamen/src/gruppeeksamen/Controller/LoggetInnController.java
+++ gruppeeksamen/src/gruppeeksamen/Controller/LoggetInnController.java
@@ -74,34 +74,6 @@ public class LoggetInnController {
     }
 
     @FXML
-    private void sorterePaaDato(ActionEvent event) {
-        ObservableList<String> forSorteringsListe = FXCollections.observableArrayList();
-        DataHandler.hentDataHele("src/gruppeeksamen/arrangementer.csv", forSorteringsListe);
-        ObservableList<String> sortertListe = FXCollections.observableArrayList();
-        ArrayList la = new ArrayList<>(forSorteringsListe);
-        ArrayList lo = (ArrayList) la.get(0);
-        int aar = Integer.parseInt(lo.get(3).toString().split("\\.")[0]);
-        int maaned = Integer.parseInt(lo.get(3).toString().split("\\.")[1]);
-        int dato = Integer.parseInt(lo.get(3).toString().split("\\.")[2]);
-        String utskrift = lo.get(3) + ": " + lo.get(0) + " (" + lo.get(4) + ")";
-        for (int i=1; i<forSorteringsListe.size();i++) {
-            //Legger til dato, navn på arrangement og type arrangement i array
-            ArrayList loo = (ArrayList) la.get(i);
-            int aaret = Integer.parseInt(loo.get(3).toString().split("\\.")[0]);
-            int maaneden = Integer.parseInt(loo.get(3).toString().split("\\.")[1]);
-            int datoen = Integer.parseInt(loo.get(3).toString().split("\\.")[2]);
-            if (aar<=aaret) {
-                if (maaned<=maaneden) {
-                    if (dato<datoen) {
-                        utskrift = loo.get(0).toString();
-                    }
-                }
-            }
-        }
-        System.out.println(utskrift);
-    }
-
-    @FXML
     private void leggTilArrangement(ActionEvent event) {
         //Lukker nåværende vindu og sender til scenen hvor man kan legge til arrangement
         Stage stagen = (Stage) leggTilArrangement.getScene().getWindow();

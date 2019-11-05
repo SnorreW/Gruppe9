7710842830df473f5a7a40f8dfe2fc894e236bb7
jensmo2 <43669666+jensmo2@users.noreply.github.com>
2019-11-05 11:38:48 +0100

    Maven, med endret filsti

---
+++ gruppeeksamen/Test/gruppeeksamen/Modell/ArrangementerTest.java
@@ -0,0 +1,12 @@
+package gruppeeksamen.Modell;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrangementerTest {
+
+    @Test
+    void getNavn() {
+    }
+}
